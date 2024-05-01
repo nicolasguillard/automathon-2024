e4a745f52c49fa8853306c00f1669d7a448b6b76
Nicolas Guillard <nguillard@perfmeup.com>
2024-05-01 18:28:14 +0200

    no message

--- get_faces.py
+++ get_faces.py
@@ -100,7 +100,7 @@ for dataset_choice in ["train", "test", "experimental"]:
                     face = frame[box_square[1]:box_square[3], box_square[0]:box_square[2], :]
                     faces.append(face)
 
-                frames_face = torch.cat(frames_face)
+                frames_face = torch.cat(faces)
                     
                 # Save files
                 saved_tensor_path = os.path.join(save_path, file.replace(".mp4", "_face.pt"))
