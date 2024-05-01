9d271612d47171593ea4ac751549cae7c69dcb1b
Nicolas Guillard <nguillard@perfmeup.com>
2024-05-01 15:53:22 +0200

    no message

--- facenet_test2.py
+++ facenet_test2.py
@@ -6,54 +6,43 @@ from PIL import Image
 import numpy as np
 from matplotlib import pyplot as plt
 from tqdm.notebook import tqdm
+import torch
+import torchvision
 
 # Create face detector
-mtcnn = MTCNN(select_largest=False, post_process=False, device='cuda')
+mtcnn = MTCNN(select_largest=False, post_process=False, margin= 40, device='cuda')
 
 # Load a video
 dataset_dir = "/raid/datasets/hackathon2024"
 video_path = os.path.join(dataset_dir, "experimental_dataset", "rdftmwfljq.mp4")
+save_tensor_path = os.path.join("/raid/home/automathon_2024/account10/", 'frames_face.pt')
+save_video_path = os.path.join("/raid/home/automathon_2024/account10/", 'frames_face.mp4')
 
-v_cap = cv2.VideoCapture(video_path)
+def capture_face(video_path, saved_tensor_path="", saved_video_path=""):
+    v_cap = cv2.VideoCapture(video_path)
 
-# Loop through video
-batch_size = 32
-frames = []
-boxes = []
-landmarks = []
-view_frames = []
-view_boxes = []
-view_landmarks = []
-for _ in tqdm(range(v_len)):
-    
-    # Load frame
-    success, frame = v_cap.read()
-    if not success:
-        continue
-        
-    # Add to batch, resizing for speed
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    frame = Image.fromarray(frame)
-    frame = frame.resize([int(f * 0.25) for f in frame.size])
-    frames.append(frame)
-    
+    # Loop through video
+    frames = []
+    v_len = int(v_cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    for i in tqdm(range(v_len)):
+        print(i)
+        # Load frame
+        success, frame = v_cap.read()
+        if not success:
+            continue
+            
+        # Add to batch, resizing for speed
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame = Image.fromarray(frame)
+        frames.append(frame)
+
+    frames_face = mtcnn(frames)
     # When batch is full, detect faces and reset batch list
-    if len(frames) >= batch_size:
-        batch_boxes, _, batch_landmarks = mtcnn.detect(frames, landmarks=True)
-        boxes.extend(batch_boxes)
-        landmarks.extend(batch_landmarks)
-        
-        view_frames.append(frames[-1])
-        view_boxes.append(boxes[-1])
-        view_landmarks.append(landmarks[-1])
-        
-        frames = []
+    if saved_tensor_path:
+        torch.save(frames_face, saved_tensor_path)
+    if saved_video_path:
+        torchvision.io.write_video(frames_face, saved_video_path)
 
-# Visualize
-fig, ax = plt.subplots(3, 3, figsize=(18, 12))
-for i in range(9):
-    ax[int(i / 3), i % 3].imshow(view_frames[i])
-    ax[int(i / 3), i % 3].axis('off')
-    for box, landmark in zip(view_boxes[i], view_landmarks[i]):
-        ax[int(i / 3), i % 3].scatter(*np.meshgrid(box[[0, 2]], box[[1, 3]]), s=8)
-        ax[int(i / 3), i % 3].scatter(landmark[:, 0], landmark[:, 1], s=6)
+
+capture_face(video_path, saved_tensor_path=save_tensor_path, saved_video_path=save_video_path)
+
